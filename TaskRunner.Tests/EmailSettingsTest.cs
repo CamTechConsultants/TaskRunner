b3372eef3b60926d75d102be7d2ef1cbd3ee57c1
John Hall <john.hall@camtechconsultants.com>
2017-09-14 11:19:19 +0000

    Add EmailSettings unit tests

---
+++ TaskRunner.Tests/EmailSettingsTest.cs
@@ -0,0 +1,91 @@
+﻿/*
+ * John Hall <john.hall@camtechconsultants.com>
+ * Copyright (c) Cambridge Technology Consultants Ltd. All rights reserved.
+ */
+
+using System.Net.Mail;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace TaskRunner.Tests
+{
+	[TestClass]
+	public class EmailSettingsTest
+	{
+		private static readonly MailAddress ExampleAddress1 = new MailAddress("joe.bloggs@example.com");
+		private static readonly MailAddress ExampleAddress2 = new MailAddress("fred.bloggs@example.com");
+
+
+		[TestMethod]
+		public void IsValid_AllFieldsPresent_ReturnsTrue()
+		{
+			var settings = new EmailSettings(host: "mail.example.com", from: ExampleAddress1, to: ExampleAddress2);
+			bool isValid = settings.IsValid;
+			isValid.ShouldBe(true);
+		}
+
+		[TestMethod]
+		public void IsValid_NoHost_ReturnsFalse()
+		{
+			var settings = new EmailSettings(from: ExampleAddress1, to: ExampleAddress2);
+			bool isValid = settings.IsValid;
+			isValid.ShouldBe(false);
+		}
+
+		[TestMethod]
+		public void IsValid_NoFromAddress_ReturnsFalse()
+		{
+			var settings = new EmailSettings(host: "mail.example.com", to: ExampleAddress2);
+			bool isValid = settings.IsValid;
+			isValid.ShouldBe(false);
+		}
+
+		[TestMethod]
+		public void IsValid_NoToAddress_ReturnsFalse()
+		{
+			var settings = new EmailSettings(host: "mail.example.com", from: ExampleAddress1);
+			bool isValid = settings.IsValid;
+			isValid.ShouldBe(false);
+		}
+
+
+		[TestMethod]
+		public void MergeIn_ThisFieldsAllNull_AllFieldsReplaced()
+		{
+			var settings1 = new EmailSettings();
+			var settings2 = new EmailSettings(host: "mail.example.com", from: ExampleAddress1, to: ExampleAddress2);
+
+			var result = settings1.MergeIn(settings2);
+
+			result.Host.ShouldBe("mail.example.com");
+			result.From.ShouldBe(ExampleAddress1);
+			result.To.ShouldBe(ExampleAddress2);
+		}
+
+		[TestMethod]
+		public void MergeIn_ThisFieldsAllSetAndOtherFieldsAllNull_AllFieldsLeftUnchanged()
+		{
+			var settings1 = new EmailSettings(host: "mail.example.com", from: ExampleAddress1, to: ExampleAddress2);
+			var settings2 = new EmailSettings();
+
+			var result = settings1.MergeIn(settings2);
+
+			result.Host.ShouldBe("mail.example.com");
+			result.From.ShouldBe(ExampleAddress1);
+			result.To.ShouldBe(ExampleAddress2);
+		}
+
+		[TestMethod]
+		public void MergeIn_ThisFieldsAllSetAndOtherFieldsAllSet_ThisFieldsAllReplaced()
+		{
+			var settings1 = new EmailSettings(host: "mail.example.com", from: ExampleAddress1, to: ExampleAddress1);
+			var settings2 = new EmailSettings(host: "mail.example.com", from: ExampleAddress2, to: ExampleAddress2);
+
+			var result = settings1.MergeIn(settings2);
+
+			result.Host.ShouldBe("mail.example.com");
+			result.From.ShouldBe(ExampleAddress2);
+			result.To.ShouldBe(ExampleAddress2);
+		}
+	}
+}
---
+++ TaskRunner.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaskRunner.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cambridge Technology Consultants Ltd.")]
+[assembly: AssemblyProduct("TaskRunner.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017 Cambridge Technology Consultants Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b38d2e38-2f2c-4db2-acf3-1bfcd2285074")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
